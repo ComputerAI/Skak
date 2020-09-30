83c4425dae419e2171b3eaf1c821a7876e5636ff
niko5297 <s185082@student.dtu.dk>
2020-09-30 08:49:27 +0200

    Added basic classes

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+class Game:
+    #TODO: Add game logic here
+    print("Game")
+
+
+
+class Board:
+    #TODO: Add board logic
+    print("Board")
+
+
+
+class Piece: 
+    #TODO: Add piece fields
+    print("Piece")
+
+
+
+
+if __name__ == "__main__":
+    #TODO: Call game logic and start new game
+    print("Main")
