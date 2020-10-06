2bcfa05d3956cfde73473ab80146bbdf6e937363
niko5297 <s185082@student.dtu.dk>
2020-10-06 14:43:43 +0200

    Added method for evaluating board and alphabeta algo

--- main.py
+++ main.py
@@ -9,42 +9,14 @@ pip install PyQt5
 import chess
 import chess.svg
 
-from PyQt5.QtSvg import QSvgWidget
-from PyQt5.QtWidgets import QApplication, QWidget
+def evaluate():
+    print("Evaluating board")
 
-class Game:
-    #TODO: Add game logic here
-    print("Game")
-
-
+def alphabeta(alpha, beta):
+    print("Running AlphaBeta algorithm")
 
 if __name__ == "__main__":
     board = chess.Board()
     print(board)
-
-
-
-
-#This method can show a board ;)
-"""
-class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.setGeometry(100, 100, 740, 740)
-
-        self.widgetSvg = QSvgWidget(parent=self)
-        self.widgetSvg.setGeometry(10, 10, 720, 720)
-
-        self.chessboard = chess.Board()
-
-        self.chessboardSvg = chess.svg.board(self.chessboard).encode("UTF-8")
-        self.widgetSvg.load(self.chessboardSvg)
-
-if __name__ == "__main__":
-    app = QApplication([])
-    window = MainWindow()
-    window.show()
-    app.exec()
-
-"""
+    evaluate()
+    alphabeta(1,2)
