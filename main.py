d455103454107407ae7a4e0959194d5b98695da6
s185114 <s185114@student.dtu.dk>
2020-10-27 18:06:04 +0100

    Pieces score and autoplay

--- main.py
+++ main.py
@@ -20,7 +20,25 @@ c=0
 def score(player,depth):
     global c
     c += 1
-    return 0
+                        #piecetype
+    pawn = 1.0          #1
+    knight = 3.05       #2
+    bishop = 3.33       #3
+    rook = 5.63         #4
+    queen = 9.5         #5
+    king = 200          #6
+    pieces = [0,pawn,knight,bishop,rook,queen,king]
+
+    mul = 1
+    color = player
+    diff = 0
+    for _ in range(2):
+        for j in range(1,7):
+            diff += mul*len(board.pieces(j,color))*pieces[j]
+        color = not color
+        mul = -1
+    
+    return diff
 
 def alphabeta(alpha, beta, depth, player):
     if depth == 0 or board.is_game_over(): return score(player, depth)
@@ -60,25 +78,37 @@ def ab(depth, player):
             mov=val
     return mov
 
-'''
-def play(Game, player):
-    if player is 'X': E='O'
-    else: E = 'X'
-    while not Game.done():
-        if Game.turn == player:
-            T.printBoard(Game.board)
-            Game.move(input("Your move"))
-        else: Game.move(ab(Game, 9, E))
-    if T.won(Game.board):
-        if Game.turn != player: print(f"Player {player} Wins")
+
+def play(player):
+    E = not player
+    while not board.is_game_over():
+        if board.turn == player:
+            print(board.unicode())
+            print([str(i) for i in board.legal_moves])
+            print()
+            board.push_san(input("Your move: \n"))
+        else: board.push(ab(3, E))
+    
+    if not board.is_stalemate():
+        if board.turn != player: print(f"Player {player} Wins")
         else: print(f"Player {E} Wins")
     else: print("It was a tie")
-'''
+
+def autoplay(depth):
+    while not board.is_game_over():
+        if board.turn == True: board.push(ab(depth, True))
+        else: board.push(ab(depth, False))
+        print(board.unicode(),'\n')
+    if not board.is_stalemate():
+        if board.turn: print(f"Player white Wins")
+        else: print(f"Player black Wins")
+    else: print("It was a tie")
+#'''
 
 
 if __name__ == "__main__":
     board = chess.Board()
-    board._set_board_fen("r7/1k2N1p1/3R4/3R4/2Q5/8/1K6/8")
-    print(ab(6,True))
+    #board._set_board_fen("r7/1k2N1p1/3R4/3R4/2Q5/8/1K6/8")
+    #print(ab(6,True))
+    autoplay(3)
     print(c)
-    print(board.is_game_over())
